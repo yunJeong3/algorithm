e604acc60ebaadf268c1f0892e66a98b5312e78d
yun <dldbswjd7879@gmail.com>
2023-09-17 15:44:16 +0900

    commit) 0917

--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class PalindromeString {
     public boolean solution(String str) {
-        boolean isTrue = false;
-        StringBuilder check = new StringBuilder();
-        for(int i=str.length()-1; i>=0; i--) {
-            check.append(str.charAt(i));
+
+        int lt = 0;
+        int rt = str.length()-1;
+
+        while(lt < rt) {
+            if(str.charAt(lt) != str.charAt(rt)) return false;
+            lt++;
+            rt--;
         }
-
-        if(str.contentEquals(check.toString())) isTrue = true;
-
-        return isTrue;
+        return true;
     }
     public static void main(String[] args) {
         // 앞에서 읽을 때랑 뒤에서 읽을 때 같은 문자열인 회문 문자열 체크
