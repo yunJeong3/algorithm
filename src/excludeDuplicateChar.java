500b9606b5391b69fcf903f24c593b766fc31fc2
yun <dldbswjd7879@gmail.com>
2023-09-17 11:51:10 +0900

    commit) 0917

--- src/excludeDuplicateChar.java
+++ src/excludeDuplicateChar.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
 public class excludeDuplicateChar {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String strIn = scan.next();
-        String result = "";
+    public String solution(String strIn) {
+        StringBuilder str = new StringBuilder();
 
+        // 1. 이중for문 사용해서 첫 값을 넣어두고 끝까지 체크했을 때 중복되지 않으면 str에 값을 넣어줌
+//        str.append(strIn.charAt(0));
+//        for(int i=1; i<strIn.length(); i++) {
+//            boolean dupl = false;
+//            for(int j=0; j<str.length(); j++) {
+//                if(strIn.charAt(i) == str.charAt(j)) dupl = true;
+//            }
+//            if(!dupl) str.append(strIn.charAt(i));
+//        }
+
+        // 2. for문과 indexOf를 사용해서 i값과 indexOf로 찾은 값이 다르면 출력
+        // 시간복잡도O(N^2) 공간복잡도 O(N)
         for(int i=0; i<strIn.length(); i++) {
-            // i == indexOf : 중복되는 글자 없음, 중복 없을 때만 result에 추가
-            if(i == strIn.indexOf(strIn.charAt(i)))
-                result += strIn.charAt(i);
+            if(i == strIn.indexOf(strIn.charAt(i))) str.append(strIn.charAt(i));
         }
 
-        System.out.println(result);
+        return str.toString();
+    }
+    public static void main(String[] args) {
+        excludeDuplicateChar T = new excludeDuplicateChar();
+        Scanner scan = new Scanner(System.in);
+        String strIn = scan.next();
+
+        System.out.println(T.solution(strIn));
 
     }
 }
