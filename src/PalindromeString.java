8a76e92f8242995124c15d6cffb897279a107ae6
yun <dldbswjd7879@gmail.com>
2023-09-17 13:34:02 +0900

    commit) 0917

--- src/PalindromeString.java
+++ src/PalindromeString.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 
 public class PalindromeString {
+    public boolean solution(String str) {
+        boolean isTrue = false;
+        StringBuilder check = new StringBuilder();
+        for(int i=str.length()-1; i>=0; i--) {
+            check.append(str.charAt(i));
+        }
+
+        if(str.contentEquals(check.toString())) isTrue = true;
+
+        return isTrue;
+    }
     public static void main(String[] args) {
+        // 앞에서 읽을 때랑 뒤에서 읽을 때 같은 문자열인 회문 문자열 체크
+        PalindromeString T = new PalindromeString();
         Scanner scan = new Scanner(System.in);
         String str = scan.next().toUpperCase();
 
-        String reverseStr = "";
-
-        for(int i=str.length()-1 ; i >= 0; i--) {
-            reverseStr += str.charAt(i);
-        }
-
-        System.out.println(str);
-        System.out.println(reverseStr);
-
-        // 앞으로, 뒤로 모두 같은 문잔지 확인
-        if(str.equals(reverseStr)) System.out.println("YES");
+        if(T.solution(str)) System.out.println("YES");
         else System.out.println("NO");
+
     }
 }
