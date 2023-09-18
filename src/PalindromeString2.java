c9402f5c9f89b2aa250fedb3c5a05845c4645945
yun <dldbswjd7879@gmail.com>
2023-09-18 16:07:07 +0900

    commit) 0918

--- src/PalindromeString2.java
+++ src/PalindromeString2.java
@@ -2,25 +2,28 @@ import java.util.Scanner;
 
 public class PalindromeString2 {
     public boolean solution(String str) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for(char c : str.toCharArray()) {
-            if(Character.isAlphabetic(c)) result += c;
-        }
-        result = result.toUpperCase();
-        String reverse = "";
-
-        for(int i=result.length()-1; i>=0; i--) {
-            reverse += result.charAt(i);
+            if(Character.isAlphabetic(c)) result.append(Character.toUpperCase(c));
         }
 
-        return reverse.equals(result);
+        int lt = 0;
+        int rt = result.length()-1;
+
+        while(lt < rt) {
+            if(result.charAt(lt) != result.charAt(rt)) return false;
+            lt++;
+            rt--;
+        }
+
+        return true;
     }
     public static void main(String[] args) {
         // 앞, 뒤에서 읽을 때 같은 문자열인지 체크
         PalindromeString2 T = new PalindromeString2();
         Scanner scan = new Scanner(System.in);
-        String str = scan.nextLine();
 
+        String str = scan.nextLine();
         if(T.solution(str)) System.out.println("YES");
         else System.out.println("NO");
 
