a8039a2db03f17e825807d74bcf51303b2381dd9
yun <dldbswjd7879@gmail.com>
2023-09-20 22:24:16 +0900

    commit) 0920

--- src/DecimalCount.java
+++ src/DecimalCount.java
@@ -1,25 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DecimalCount {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
+    public int solution(int max) {
+        int resultCnt = 0;
+        int[] arr = new int[max+1];
 
-        int decimalCount = 0;
-        for(int i=2; i<=N; i++) {
-            int count = 0;
-            for(int j=2; j<=9  ; j++) {
-                if(j > i) break;
-                if(j == i) continue;
-                if(i % j == 0) {
-                    count++;
-                    break;
-                }
+        for(int i=2; i<=max; i++) {
+            // i번째가 0이면 count증가.
+            if(arr[i] == 0) {
+                resultCnt++;
+                // i만큼(i의배수)돌아야함. 2의배수는 2씩 증가, 3의배수는 3씩증가
+                for(int j=i; j<=max; j=j+i) arr[j] = 1;
             }
-
-            if(count == 0) decimalCount++;
         }
 
-        System.out.println(decimalCount);
+        return resultCnt;
+    }
+    public static void main(String[] args) {
+        // 에라토스테네스체를 통해 입력받은 수까지의 소수 갯수 세기.
+        DecimalCount T = new DecimalCount();
+        Scanner scan = new Scanner(System.in);
+
+        int max = scan.nextInt();
+        System.out.println(T.solution(max));
     }
 }
