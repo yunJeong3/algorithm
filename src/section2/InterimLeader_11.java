77dd5edda791e59978e54f3f6c71fd3951cc10b8
yun <dldbswjd7879@gmail.com>
2023-09-25 01:50:12 +0900

    commit) 0925

--- src/section2/InterimLeader_11.java
+++ src/section2/InterimLeader_11.java
@@ -3,54 +3,42 @@ package section2;
 import java.util.Scanner;
 
 public class InterimLeader_11 {
-    public int[] solution(int stuLen, int[][] arr) {
-//        int result = 0;
-        int selY = 0;
-        int selX = 0;
-        int[] resultArr = new int[stuLen];
+    public int solution(int stuLen, int[][] arr) {
+        int result = 0, max = Integer.MIN_VALUE;
 
-        for(int n=0; n<stuLen; n++) {
-            for(int x=0; x<5; x++) {
-                int target = arr[selY][selX];
-
-                for(int y=0; y<stuLen; y++) {
-                    if(selY == y && selX == x) {
-                        continue;
-                    }
-                    if(arr[y][x] == target) {
-                        resultArr[n]++;
+        for(int i=1; i<=stuLen; i++) {
+            int cnt = 0;
+            for(int j=1; j<=stuLen; j++) {
+                for(int k=1; k<=5; k++) {
+                    if(arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
                     }
                 }
-                if(selX >= 4) selX = 0;
-                else selX++;
             }
-            selY++;
+            if(max < cnt) {
+                max = cnt;
+                result = i;
+            }
         }
-        return resultArr;
+
+        return result;
     }
     public static void main(String[] args) {
+        //
         InterimLeader_11 T = new InterimLeader_11();
         Scanner scan = new Scanner(System.in);
 
         int stuLen = scan.nextInt();
-        int[][] arr = new int[stuLen][5];
-
-        for(int i=0; i<stuLen; i++) {
-            for(int j=0; j<5; j++) {
+        int[][] arr = new int[stuLen+1][6];
+        for(int i=1; i<=stuLen; i++) {
+            for(int j=1; j<=5; j++) {
                 arr[i][j] = scan.nextInt();
             }
         }
 
-        int[] resultArr = T.solution(stuLen, arr);
-        int top = resultArr[0];
-        int idx = 0;
-        for(int i=1; i<resultArr.length; i++) {
-            if(top < resultArr[i]) {
-                top = resultArr[i];
-                idx = i;
-            }
-        }
-        System.out.println(idx+1);
+        System.out.println(T.solution(stuLen, arr));
+
 
     }
 }
