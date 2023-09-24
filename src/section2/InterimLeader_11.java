16ae51c0e236dc9bdbe85ef83c0ca8fd68b91abc
yun <dldbswjd7879@gmail.com>
2023-09-24 23:51:32 +0900

    commit) 0924

--- src/section2/InterimLeader_11.java
+++ src/section2/InterimLeader_11.java
@@ -3,11 +3,30 @@ package section2;
 import java.util.Scanner;
 
 public class InterimLeader_11 {
-    public int solution(int stuLen, int[][] arr) {
-        int result = 0;
+    public int[] solution(int stuLen, int[][] arr) {
+//        int result = 0;
+        int selY = 0;
+        int selX = 0;
+        int[] resultArr = new int[stuLen];
 
+        for(int n=0; n<stuLen; n++) {
+            for(int x=0; x<5; x++) {
+                int target = arr[selY][selX];
 
-        return result;
+                for(int y=0; y<stuLen; y++) {
+                    if(selY == y && selX == x) {
+                        continue;
+                    }
+                    if(arr[y][x] == target) {
+                        resultArr[n]++;
+                    }
+                }
+                if(selX >= 4) selX = 0;
+                else selX++;
+            }
+            selY++;
+        }
+        return resultArr;
     }
     public static void main(String[] args) {
         InterimLeader_11 T = new InterimLeader_11();
@@ -22,7 +41,16 @@ public class InterimLeader_11 {
             }
         }
 
-        System.out.println(T.solution(stuLen, arr));
+        int[] resultArr = T.solution(stuLen, arr);
+        int top = resultArr[0];
+        int idx = 0;
+        for(int i=1; i<resultArr.length; i++) {
+            if(top < resultArr[i]) {
+                top = resultArr[i];
+                idx = i;
+            }
+        }
+        System.out.println(idx+1);
 
     }
 }
