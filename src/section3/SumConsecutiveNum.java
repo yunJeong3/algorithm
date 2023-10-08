7a4c1442cda79dfb0c7a47732e6c75af0fab26fd
yun <dldbswjd7879@gmail.com>
2023-10-08 23:58:46 +0900

    commit) 1008

---
+++ src/section3/SumConsecutiveNum.java
@@ -0,0 +1,28 @@
+package section3;
+
+import java.util.Scanner;
+
+public class SumConsecutiveNum {
+    public int solution(int target){
+        int cnt = 1;
+        int result = 0;
+        target--;
+        while(target>0) {
+            cnt++;
+            target = target - cnt;
+            if(target % cnt == 0) {
+                result++;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        SumConsecutiveNum T = new SumConsecutiveNum();
+        Scanner scan = new Scanner(System.in);
+
+        // 연속된 자연수의 합으로 target을 표현하는 방법의 가짓수 구하기
+        int target = scan.nextInt();
+        System.out.println(T.solution(target));
+
+    }
+}
