a1e2ea7a838cc18e8328c0fbf3e65733e08de542
yun <dldbswjd7879@gmail.com>
2023-10-18 01:07:19 +0900

    commit) 1018

--- src/section3/MaxLen_6.java
+++ src/section3/MaxLen_6.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class MaxLen_6 {
     public int solution(int arrLen, int changeCnt, int[] arr) {
+        /*
+        cnt: 0 -> 1로 몇 번 변경했는지 임시 저장
+        result: 최대 길이를 담을 수 저장
+        changeCnt: 0 -> 1로 변경 가능한 수
+        . rt로 index 1씩 증가하며, rt위치에 0이 있으면 cnt 증가
+        . cnt가 changeCnt를 초과하면 lt index를 1씩 증가하여 lt위치에서 0을 만나면 cnt 감소하고 lt 1번 증가 후 반복문 종료
+        . [기존 result 값]과 [rt-lt한 값에 index 감안하여 1 증가한 값]을 비교하여 큰 값을 result에 다시 저장
+            => result는 최대 길이가 담김.
+        */
         int result=0, cnt=0, lt=0;
 
         for(int rt=0; rt<arrLen; rt++) {
